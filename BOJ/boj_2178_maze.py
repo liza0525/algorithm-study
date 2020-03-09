68794d6d4913158360a8c866ad045aaa45158ef6
Yunyoung Chung <double.y.0525@gmail.com>
2020-03-09 21:28:32 +0900

    BOJ/boj_2178_maze.py(optimize)

--- BOJ/boj_2178_maze.py
+++ BOJ/boj_2178_maze.py
@@ -5,27 +5,24 @@ sys.stdin = open('../input.txt', 'r')
 ds = [(1, 0), (0, 1), (0, -1), (-1, 0)]
 
 def bfs(i, j):
-    global visited
     visited[i][j] = 1
-    queue = [(i, j)]
+    q = [(i, j)]
 
-    while queue:
-        si, sj = queue.pop()
+    while q:
+        si, sj = q.pop(0)
         for di, dj in ds:
             ni, nj = si+di, sj+dj
             if 0 <= ni < N and 0 <= nj < M:
-                if g[ni][nj] and (not visited[ni][nj] or sc[ni][nj] > sc[si][sj] + 1):
-                    sc[ni][nj] = sc[si][sj] + 1
-                    visited[ni][nj] = 1
-                    queue.append((ni, nj))
-
+                if g[ni][nj] and not visited[ni][nj]:
+                    visited[ni][nj] = visited[si][sj] + 1
+                    q.append((ni, nj))
+            elif ni == N and nj == M:
+                break
 
 N, M = map(int, input().split())
 g = [list(map(int, input())) for _ in range(N)]
-sc = [[N*M+1]*M for _ in range(N)]
-sc[0][0] = 1
 visited = [[0]*M for _ in range(N)]
 
 bfs(0, 0)
 
-print(sc[N-1][M-1])
+print(visited[N-1][M-1])
