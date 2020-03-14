b2654e8dddea8b10edd1c871cfbd3b22c7c12425
Yunyoung Chung <double.y.0525@gmail.com>
2020-03-14 16:01:46 +0900

    BOJ/boj_1148_RGBstreet.py

--- BOJ/boj_1148_RGBstreet.py
+++ BOJ/boj_1148_RGBstreet.py
@@ -2,5 +2,15 @@ import sys
 sys.stdin = open('../input.txt', 'r')
 
 N = int(input())
-color_p = [list(map(int, input().split())) for _ in range(N)]
+cp = [list(map(int, input().split())) for _ in range(N)] # 집 별 색깔 별 가격
+price = [cp[0]] + [[0 ,0, 0] for _ in range(N-1)]
 
+for i in range(1, N):
+    for j in range(3):
+        cand = []
+        for k in range(3):
+            if j != k:
+                cand.append(price[i-1][k])
+        price[i][j] = cp[i][j] + min(cand)
+
+print(min(price[N-1]))
