1ea48137f765258f120090af42f56900d9fa0761
Yunyoung Chung <double.y.0525@gmail.com>
2020-03-16 22:02:59 +0900

    BOJ/boj_1932_integer_triangle.py

---
+++ BOJ/boj_1932_integer_triangle.py
@@ -0,0 +1,13 @@
+import sys
+sys.stdin = open('../input.txt', 'r')
+
+N = int(input())
+
+g = [list(map(int, input().split())) for _ in range(N)]
+mp = [[0] * N for _ in range(N-1)] + [g[N-1]]
+
+for i in range(N-2, -1, -1):
+    for j in range(i+1):
+        mp[i][j] = g[i][j] + max(mp[i+1][j], mp[i+1][j+1])
+
+print(mp[0][0])
