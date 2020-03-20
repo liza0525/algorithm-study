6418342adc76b406b69266834c9d92a973b462d8
Yunyoung Chung <double.y.0525@gmail.com>
2020-03-20 23:21:25 +0900

    BOJ/boj_2294_coin2.py

---
+++ BOJ/boj_2294_coin2.py
@@ -0,0 +1,17 @@
+import sys
+sys.stdin = open('../input.txt', 'r')
+
+N, K = map(int, input().split())
+values = sorted([int(input()) for _ in range(N)])
+# print(values)
+nums = [100001] * (K+1)
+nums[0] = 0
+
+for value in values:
+    for j in range(value, K+1):
+        nums[j] = min(nums[j], nums[j-value]+1)
+
+if nums[-1] != 100001:
+    print(nums[-1])
+else:
+    print(-1)
