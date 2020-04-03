7d23698b1538878f26b10ddaf4c1c5928d0fa91b
Yunyoung Chung <double.y.0525@gmail.com>
2020-04-03 22:34:30 +0900

    BOJ/boj_5567_wedding.py

--- BOJ/boj_5567_wedding.py
+++ BOJ/boj_5567_wedding.py
@@ -1,12 +1,16 @@
-import sys
-sys.stdin = open('../input.txt', 'r')
-
 N = int(input())
 M = int(input())
 g = [[0] * (N+1) for _ in range(N+1)]
-
+visited = [0] * (N+1)
 for _ in range(M):
     a, b = map(int, input().split())
     g[a][b], g[b][a] = 1, 1
 
-print(g)
+for i in range(2, N+1):
+    if g[1][i]:
+        visited[i] = 1
+        for j in range(2, N+1):
+            if g[i][j] and not visited[j]:
+                visited[j] = 1
+
+print(sum(visited))
