2ee49c652653fe812c9c5f8aeb0dbcec54ca8fd2
Yunyoung Chung <double.y.0525@gmail.com>
2020-04-14 23:33:52 +0900

    BOJ/boj_6603_lotto.py(solved)

--- BOJ/boj_6603_lotto.py
+++ BOJ/boj_6603_lotto.py
@@ -1,9 +1,29 @@
 import sys
 sys.stdin = open('../input.txt', 'r')
 
-data = []
+
+def combi(arr, d, next):
+    if d == 6:
+        print(' '.join(map(str, arr)))
+    else:
+        for i in range(next, len(data)):
+            if data[i] not in arr and sorted(arr + [data[i]]) not in memo:
+                arr.append(data[i])
+                memo.append(sorted(arr))
+                combi(arr, d+1, next+1)
+                arr.pop()
+
+
+data_list = []
 while True:
     now_input = input()
     if now_input == '0':
         break
-    data.append(list(now_input.split()))
+    data_list.append(list(map(int, now_input.split())))
+
+for data in data_list:
+    memo = []
+    N = data.pop(0)
+    combi([], 0, 0)
+    if data != data_list[-1]:
+        print()
