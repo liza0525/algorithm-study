d792c3c34db0ed9ff0ef3aa0271e45f04f0cc30c
Yunyoung Chung <double.y.0525@gmail.com>
2020-04-14 22:43:45 +0900

    BOJ/boj_9663_N_Queen.py(solved)

--- BOJ/boj_9663_N_Queen.py
+++ BOJ/boj_9663_N_Queen.py
@@ -1,4 +1,24 @@
-import sys
-sys.stdin = open('../input.txt', 'r')
+def n_queen(arr):
+    global count
+    if len(arr) == N:
+        count += 1
+    else:
+        for cand in range(N):
+            if cand in arr:
+                continue
+            for i in range(len(arr)):
+                if abs(cand - arr[i]) == len(arr) - i:
+                    break
+            else:
+                arr.append(cand)
+                n_queen(arr)
+                arr.pop()
+
 
 N = int(input())
+count = 0
+
+for i in range(N):
+    n_queen([i])
+
+print(count)
