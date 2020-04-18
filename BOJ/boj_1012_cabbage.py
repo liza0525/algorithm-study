7bf0fa2ab4b1e89a1dc3c80ed4f45eb9e9fb4151
Yunyoung Chung <double.y.0525@gmail.com>
2020-04-18 18:33:44 +0900

    BOJ/boj_1012_cabbage.py(solved)

--- BOJ/boj_1012_cabbage.py
+++ BOJ/boj_1012_cabbage.py
@@ -1,8 +1,39 @@
 import sys
+from pprint import pprint
 sys.stdin = open('../input.txt', 'r')
 
+ds = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+
+
+def dfs(i, j):
+    stack = [(i, j)]
+
+    while stack:
+        si, sj = stack.pop()
+        for di, dj in ds:
+            ni, nj = si+di, sj+dj
+            if 0 <= ni < M and 0 <= nj < N:
+                if g[ni][nj] and not visited[ni][nj]:
+                    stack.append((ni, nj))
+                    visited[ni][nj] = 1
+    return 1
+
+
 T = int(input())
-M, N, K = map(int, input().split())
 
 for test in range(T):
-    X, Y = map(int, input().split())
+    M, N, K = map(int, input().split())
+    g = [[0] * N for _ in range(M)]
+    visited = [[0] * N for _ in range(M)]
+    res = 0
+
+    for _ in range(K):
+        X, Y = map(int, input().split())
+        g[X][Y] = 1
+
+    for i in range(M):
+        for j in range(N):
+            if g[i][j] and not visited[i][j]:
+                res += dfs(i, j)
+
+    print(res)
