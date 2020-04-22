2efcdf98aa0edf4dcce80d3c325cf7f211cb5dd4
Yunyoung Chung <double.y.0525@gmail.com>
2020-04-22 22:54:35 +0900

    BOJ/boj_1916_min_cost.py(ing)

---
+++ BOJ/boj_1916_min_cost.py
@@ -0,0 +1,7 @@
+import sys
+sys.stdin = open('../input.txt', 'r')
+
+N = int(input())
+M = int(input())
+info = [list(map(int, input().split())) for _ in range(M)]
+start, arrive = map(int, input.split())
