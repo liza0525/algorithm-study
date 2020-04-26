d90eda7dcd1bcb791488d356fa4aa4d1523916b2
Yunyoung Chung <double.y.0525@gmail.com>
2020-04-26 21:56:52 +0900

    BOJ/boj_1003_fibonacci.py(solved)

--- BOJ/boj_1003_fibonacci.py
+++ BOJ/boj_1003_fibonacci.py
@@ -5,3 +5,13 @@ T = int(input())
 
 for _ in range(T):
     N = int(input())
+    zero = [1, 0]
+    one = [0, 1]
+
+    for i in range(2, N+1):
+        cnt_zero = zero[i-1] + zero[i-2]
+        zero.append(cnt_zero)
+        cnt_one = one[i-1] + one[i-2]
+        one.append(cnt_one)
+
+    print(zero[N], one[N])
