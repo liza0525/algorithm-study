5258faa82c741fb7eafb3311fb3acd1db387cceb
Yunyoung Chung <double.y.0525@gmail.com>
2020-05-03 20:16:10 +0900

    BOJ/boj_7568_body_value.py(solved)

--- BOJ/boj_7568_body_value.py
+++ BOJ/boj_7568_body_value.py
@@ -2,8 +2,18 @@ import sys
 sys.stdin = open('../input.txt', 'r')
 
 T = int(input())
-res = []
+info, res = [], [0] * T
 for _ in range(T):
     x, y = map(int, input().split())
+    info.append((x, y))
 
-print(res, end=' ')
+for i in range(T):
+    rank = 1
+    x1, y1 = info[i]
+    for j in range(T):
+        if i == j: continue
+        x2, y2 = info[j]
+        if x1 < x2 and y1 < y2: rank += 1
+    res[i] = rank
+
+print(' '.join(map(str, res)))
