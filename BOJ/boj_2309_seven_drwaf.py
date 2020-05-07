e018db2ca60a7a3fa82ed41b6c47271adff5f694
Yunyoung Chung <double.y.0525@gmail.com>
2020-05-08 00:54:51 +0900

    BOJ/boj_2309_seven_drwaf.py(solved)

--- BOJ/boj_2309_seven_drwaf.py
+++ BOJ/boj_2309_seven_drwaf.py
@@ -1,4 +1,21 @@
 import sys
 sys.stdin = open('../input.txt', 'r')
 
+def solve(d, next):
+    global real
+    if d == 7:
+        if sum(real) == 100:
+            print('\n'.join(map(str, sorted(real))))
+            sys.exit(0)
+    else:
+        for i in range(next, 9):
+            if sum(real) <= 100:
+                real.append(heights[i])
+                solve(d+1, next+1)
+                real.pop()
+
+
 heights = [int(input()) for _ in range(9)]
+real = []
+
+solve(0, 0)
