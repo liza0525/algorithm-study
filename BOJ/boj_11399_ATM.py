f39952a45384e375a69b6a41a827080dbf03615f
Yunyoung Chung <double.y.0525@gmail.com>
2020-05-07 21:31:27 +0900

    BOJ/boj_11399_ATM.py(solved)

--- BOJ/boj_11399_ATM.py
+++ BOJ/boj_11399_ATM.py
@@ -2,4 +2,9 @@ import sys
 sys.stdin = open('../input.txt', 'r')
 
 N = int(input())
-min_wd = list(map(int, input().split()))
+wd = list(map(int, input().split()))
+wd.sort()
+res = 0
+for i in range(N):
+    res += wd[i] * (N-i)
+print(res)
