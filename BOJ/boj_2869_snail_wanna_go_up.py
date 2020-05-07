f91a50c747a96b14c2eed35ae08c0969154759a8
Yunyoung Chung <double.y.0525@gmail.com>
2020-05-07 23:59:48 +0900

    BOJ/boj_2869_snail_wanna_go_up.py(solved)

--- BOJ/boj_2869_snail_wanna_go_up.py
+++ BOJ/boj_2869_snail_wanna_go_up.py
@@ -2,3 +2,10 @@ import sys
 sys.stdin = open('../input.txt', 'r')
 
 A, B, V = map(int, input().split())
+
+days = 0
+diff = A - B
+
+days = (V - B) / diff
+
+print(int(days) if days == int(days) else int(days+1))
