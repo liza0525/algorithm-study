f6e97b54aa8da328ddbd51a37a3429b1990a39c8
Yunyoung Chung <double.y.0525@gmail.com>
2020-05-12 22:19:15 +0900

    BOJ/boj_10815_number_card.py(solved)

--- BOJ/boj_10815_number_card.py
+++ BOJ/boj_10815_number_card.py
@@ -5,3 +5,12 @@ N = int(input())
 answer = list(map(int, input().split()))
 M = int(input())
 predict = list(map(int, input().split()))
+p = dict()
+for pn in predict:
+    p.update({pn: 0})
+
+for a in answer:
+    if a in p.keys():
+        p[a] = 1
+
+print(' '.join(map(str, p.values())))
