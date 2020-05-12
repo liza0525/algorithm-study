60cb8443ae77e023e10b719b4ad7ebc1e594921d
Yunyoung Chung <double.y.0525@gmail.com>
2020-05-12 22:19:37 +0900

    BOJ/boj_2606_virus.py(solved)

--- BOJ/boj_2606_virus.py
+++ BOJ/boj_2606_virus.py
@@ -2,5 +2,23 @@ import sys
 sys.stdin = open('../input.txt', 'r')
 
 N = int(input())
-M = int(input())
-info = [list(map(int, input().split())) for _ in range(M)]
+E = int(input())
+visited = [0] * (N+1)
+g = [[0] * (N+1) for _ in range(N+1)]
+
+for _ in range(E):
+    v1, v2 = map(int, input().split())
+    g[v1][v2], g[v2][v1] = 1, 1
+
+stack = []
+stack.append(1)
+
+while stack:
+    v = stack.pop()
+    if not visited[v]:
+        visited[v] = 1
+        for i in range(N+1):
+            if g[v][i] and not visited[i]:
+                stack.append(i)
+
+print(sum(visited)-1)
