ff3687accc07e5be15b6149f650582ae1480da62
Yunyoung Chung <double.y.0525@gmail.com>
2020-05-29 22:49:12 +0900

    BOJ/boj_9019_DSLR.py(ing)

--- BOJ/boj_9019_DSLR.py
+++ BOJ/boj_9019_DSLR.py
@@ -1,6 +1,37 @@
 import sys
 sys.stdin = open('../input.txt', 'r')
 
+import collections
+
+def D(num):
+    return (num * 2) // 10000, 'D'
+
+
+def S(num):
+    return num - 1 if num != 0 else 9999, 'S'
+
+
+def L(num):
+    str_num = str(num)
+    return int(str_num[1:] + str_num[0]), 'L'
+
+
+def R(num):
+    str_num = str(num)
+    return int(str_num[-1] + str_num[:-1]), 'R'
+
+
+def bfs(num):
+    queue = collections.deque()
+    queue.append(num)
+    res = ''
+
+    while queue:
+        snum = queue.popleft()
+        for nnum, char in [D(snum), S(snum), L(snum), R(snum)]:
+            
+
+
 T = int(input())
 for _ in range(T):
     A, B = map(int, input().split())
