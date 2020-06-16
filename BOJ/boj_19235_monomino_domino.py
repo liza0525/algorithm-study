9d6da90582ae9490a4d029beb469d2fb6a407065
Yunyoung Chung <double.y.0525@gmail.com>
2020-06-16 14:32:47 +0900

    BOJ/boj_19235_monomino_domino.py(ing)

--- BOJ/boj_19235_monomino_domino.py
+++ BOJ/boj_19235_monomino_domino.py
@@ -1,5 +1,13 @@
 import sys
 sys.stdin = open('../input.txt', 'r')
 
+
+def play():
+    pass
+
+
 N = int(input())
 info = [list(map(int, input().split())) for _ in range(N)]
+
+green_g = [[0] * 4 for _ in range(6)]
+blue_g = [[0] * 4 for _ in range(6)]
