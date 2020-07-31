370b80fff907e56cea1235b7fdc1fcae0d0b9319
Yunyoung Chung <double.y.0525@gmail.com>
2020-08-01 00:14:39 +0900

    programmers coding test(BFS_DFS - 네트워크 / java)

---
+++ Programmers/BFS_DFS/네트워크.java
@@ -0,0 +1,37 @@
+class Solution {
+    
+    public static void main(String[] args) {
+        int a = 3;
+        int[][] b = {{1, 1, 0}, {1, 1, 0}, {0, 0, 1}};
+        System.out.println(solution(a, b));
+    }
+
+
+    public static void dfs(int n, int i, boolean[] visited, int[][] computers) {
+        visited[i] = true;
+        for (int j = 0 ; j < n ; j++) {
+            if (computers[j][i] == 1 && visited[j] == false) {
+                dfs(n, j, visited, computers);
+            }
+        }
+    }
+
+    public static int solution(int n, int[][] computers) {
+        int answer = 0;
+        boolean[] visited = new boolean[n];
+        for (int i = 0 ; i < n ; i++) {
+            visited[i] = false;
+        }
+
+        for (int i = 0 ; i < n ; i++){
+            for (int j = 0; j < n ; j++) {
+                if (computers[i][j] == 1 && visited[i] == false) {
+                    dfs(n, i, visited, computers);
+                    answer += 1;
+                }
+            }
+        }
+
+        return answer;
+    }
+}
