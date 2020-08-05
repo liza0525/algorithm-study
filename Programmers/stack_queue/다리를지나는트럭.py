19025bdb13d5b57d1ca4bca520522d01fbdcfacc
Yunyoung Chung <double.y.0525@gmail.com>
2020-08-06 02:25:15 +0900

    programmers coding test(stack_queue - 다리를 지나는 트럭)

--- Programmers/stack_queue/다리를지나는트럭.py
+++ Programmers/stack_queue/다리를지나는트럭.py
@@ -1,20 +1,35 @@
 from collections import deque
 
-def solution(bridge_length, weight, trucks):
+class Truck():
+    weight = 0
+    position = 0
+    def __init__(self, weight, position):
+        self.weight = weight
+        self.position = position
+
+
+def solution(bridge_length, weight, truck_weights):
     time = 0
-    trucks = deque(trucks)
-    bridge = deque([0] * bridge_length)
+    before_bridge = deque()
+    on_bridge = deque()
 
-    while trucks:
-        if trucks:
-            bridge.popleft()
-            if sum(bridge) + trucks[0] > weight:
-                bridge.append(0)
-            else:
-                bridge.append(trucks.popleft())
+    for truck_weight in truck_weights:
+        before_bridge.append(Truck(truck_weight, 1))
+
+    while before_bridge or on_bridge:
+        total_weight = 0
+        for truck in on_bridge:
+            total_weight += truck.weight
+            truck.position += 1
+
+        if on_bridge and on_bridge[0].position > bridge_length:
+            total_weight -= on_bridge.popleft().weight
+
+        if before_bridge and total_weight + before_bridge[0].weight <= weight and len(on_bridge) <= bridge_length:
+            on_bridge.append(before_bridge.popleft())
+
         time += 1
-
-    return time + bridge_length
+    return time
 
 
 print(solution(2, 10, [7, 4, 5, 6]))
