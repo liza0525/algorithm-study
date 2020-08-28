b23c52a76c875cc97def35acb4fb5e030d9daa86
Yunyoung Chung <double.y.0525@gmail.com>
2020-08-28 20:25:42 +0900

    Programmers - 기둥과 보 설치(python)

--- Programmers/kakao/기둥과보설치.py
+++ Programmers/kakao/기둥과보설치.py
@@ -2,44 +2,35 @@
 from pprint import pprint
 
 def solution(n, build_frame):
-    answer = []
-    wall = [[[0, 0, 0, 0] for _ in range(n+1)] for _ in range(n+1)]
-    # 각 위치에서 상하좌우가 연결이 되었는지 아닌지
-    up, down, left, right = 0, 1, 2, 3
+    materials = set()
 
-    for i, j, a, b in build_frame: # a : 기둥 or 보 , b : 삭제 or 설치
-        if a == 0: # 기둥인 경우
-            if b == 0: # 기둥 삭제
-                if not wall[i][j+1][right] or wall[i][j][up] or wall[i][j][down]:
-                    wall[i][j][right], wall[i][j+1][left] = 0, 0
-                elif not wall[i][j+1][up] or (wall[i-1][j+1][left] or (wall[i-1][j+1][up] and wall[i][j+1][down])):
-                    wall[i][j][right], wall[i][j+1][left] = 0, 0
-                elif not wall[i][j+1][down] or (wall[i+1][j+1][left] or (wall[i][j+1][up] and wall[i+1][j+1][down])):
-                    wall[i][j][right], wall[i][j+1][left] = 0, 0
-            elif b == 1: # 기둥 추가
-                if j == 0 or wall[i][j][left] or wall[i][j][up] or wall[i][j][down]:
-                    wall[i][j][right], wall[i][j+1][left] = 1, 1
-        elif a == 1: # 보인 경우
-            if b == 0: # 보 삭제
-                # if wall[i][j][left] or wall[i+1][j][left] or (not wall[i][j][up] and not wall[i+1][j][down]):
-                if (not wall[i][j][up] and not wall[i+1][j][down]):
-                    wall[i][j][down], wall[i+1][j][up] = 0, 0
-                elif (wall[i][j][up] and (wall[i][j][left] or wall[i-1][j][left])):
-                    if (wall[i][j][down] and (wall[i][j][left] or wall[i+1][j][left])):
-                        wall[i][j][down], wall[i+1][j][up] = 0, 0
-            elif b == 1: # 보 추가
-                if wall[i][j][left] or wall[i+1][j][left] or (wall[i][j][up] and wall[i+1][j][down]):
-                    wall[i][j][down], wall[i+1][j][up] = 1, 1
-        # pprint(wall)
+    for x, y, a, b in build_frame:
+        if b == 0: # 삭제
+            materials.remove((x, y, a))
+        elif b == 1: # 추가
+            materials.add((x, y, a))
 
-    for i in range(n+1):
-        for j in range(n+1):
-            if wall[i][j][right]: # 기둥이 연결 된 경우
-                answer.append([i, j, 0])
-            if wall[i][j][down]: # 보가 연결 된 경우
-                answer.append([i, j, 1])
-    return answer
-
+        for (xx, yy, aa) in materials:
+            if aa == 0: # 기둥인 경우
+                if yy == 0 or (xx, yy-1, 0) in materials or (xx, yy, 1) in materials or (xx-1, yy, 1) in materials:
+                    continue
+                else:
+                    if b == 0: # 삭제 한 거 다시 추가
+                        materials.add((x, y, a))
+                    elif b == 1: # 추가 한 거 다시 삭제
+                        materials.remove((x, y, a))
+                    break
+            elif aa == 1: # 보인 경우
+                if (xx, yy-1, 0) in materials or (xx+1, yy-1, 0) in materials or ((xx-1, yy, 1) in materials and (xx+1, yy, 1) in materials):
+                    continue
+                else:
+                    if b == 0: # 삭제 한 거 다시 추가
+                        materials.add((x, y, a))
+                    elif b == 1: # 추가 한 거 다시 삭제
+                        materials.remove((x, y, a))
+                    break
+                    
+    return sorted(map(list, materials), key=lambda x: (x[0], x[1], x[2]))
 
 print(solution(5, [[1,0,0,1],[1,1,1,1],[2,1,0,1],[2,2,1,1],[5,0,0,1],[5,1,0,1],[4,2,1,1],[3,2,1,1]]))
 # 답 [[1,0,0],[1,1,1],[2,1,0],[2,2,1],[3,2,1],[4,2,1],[5,0,0],[5,1,0]]
