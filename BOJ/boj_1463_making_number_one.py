ee5c9369cde97969b481cbe659d28d5f5dca8ecd
Yunyoung Chung <double.y.0525@gmail.com>
2020-09-22 01:07:18 +0900

    BOJ 1463 - 1 만들기(python/dp)

--- BOJ/boj_1463_making_number_one.py
+++ BOJ/boj_1463_making_number_one.py
@@ -1,25 +1,38 @@
 import sys
 sys.stdin = open('../input.txt', 'r')
 
-
-
-# 재귀 이용(468ms)
-def calculate(num, d):
-    global min_cnt
-    if num == 1:
-        min_cnt = min(min_cnt, d)
-    else:
-        if d < min_cnt:
-            if num % 3 == 0:
-                calculate(num // 3, d+1)
-            if num % 2 == 0:
-                calculate(num // 2, d+1)
-            calculate(num-1, d+1)
-
+## dp 이용(196ms)
 
 N = int(input())
-min_cnt = 1e9
 
-calculate(N, 0)
+dp = [0] * (N+1)
 
-print(min_cnt)
+for i in range(2, N+1):
+    dp[i] = dp[i-1] + 1
+    if i % 2 == 0:
+        dp[i] = min(dp[i], dp[i//2]+1)
+    if i % 3 == 0:
+        dp[i] = min(dp[i], dp[i//3]+1)
+
+print(dp[N])
+
+## 재귀 이용(468ms)
+# def calculate(num, d):
+#     global min_cnt
+#     if num == 1:
+#         min_cnt = min(min_cnt, d)
+#     else:
+#         if d < min_cnt:
+#             if num % 3 == 0:
+#                 calculate(num // 3, d+1)
+#             if num % 2 == 0:
+#                 calculate(num // 2, d+1)
+#             calculate(num-1, d+1)
+#
+#
+# N = int(input())
+# min_cnt = 1e9
+#
+# calculate(N, 0)
+#
+# print(min_cnt)
