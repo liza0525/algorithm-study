47d065944bb947d229237a3d2f15835952434a97
Yunyoung Chung <double.y.0525@gmail.com>
2020-09-22 01:23:17 +0900

    BOJ 1463 - 1 만들기(python/dp_repactoring)

--- BOJ/boj_1463_making_number_one.py
+++ BOJ/boj_1463_making_number_one.py
@@ -8,11 +8,12 @@ N = int(input())
 dp = [0] * (N+1)
 
 for i in range(2, N+1):
-    dp[i] = dp[i-1] + 1
-    if i % 2 == 0:
-        dp[i] = min(dp[i], dp[i//2]+1)
     if i % 3 == 0:
-        dp[i] = min(dp[i], dp[i//3]+1)
+        dp[i] = min(dp[i-1]+1, dp[i//3]+1)
+    elif i % 2 == 0:
+        dp[i] = min(dp[i-1]+1, dp[i//2]+1)
+    else:
+        dp[i] = dp[i-1] + 1
 
 print(dp[N])
 
