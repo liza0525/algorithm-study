50f7fb454a13282d6b4912ee81de6ebb0e3e4a8f
Yunyoung Chung <double.y.0525@gmail.com>
2020-09-25 23:05:49 +0900

    BOJ 11403 - 경로 찾기(python/floyd-warshall)

--- BOJ/boj_11403_finding_road.py
+++ BOJ/boj_11403_finding_road.py
@@ -1,17 +1,34 @@
 import sys
 sys.stdin = open('../input.txt', 'r')
-sys.setrecursionlimit(10**6)
 
-def dfs(node):
-    for next in range(N):
-        if visited[next] == 0 and g[node][next] == 1:
-            visited[next] = 1
-            dfs(next)
 
+## floyd-warshall
 N = int(input())
-g = [list(map(int, input().split())) for _ in range(N)]
+graph = [list(map(int, input().split())) for _ in range(N)]
 
-for node in range(N):
-    visited = [0] * N
-    dfs(node)
-    print(' '.join(map(str, visited)))
+for k in range(N):
+    for i in range(N):
+        for j in range(N):
+            if graph[i][j] == 0 and graph[i][k] and graph[k][j]:
+                graph[i][j] = 1
+
+for line in graph:
+    print(' '.join(map(str, line)))
+
+
+
+
+# ## dfs
+# def dfs(node):
+#     for next in range(N):
+#         if visited[next] == 0 and g[node][next] == 1:
+#             visited[next] = 1
+#             dfs(next)
+#
+# N = int(input())
+# g = [list(map(int, input().split())) for _ in range(N)]
+#
+# for node in range(N):
+#     visited = [0] * N
+#     dfs(node)
+#     print(' '.join(map(str, visited)))
