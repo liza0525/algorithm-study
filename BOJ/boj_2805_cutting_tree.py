387c3ef2f5420b38596b8c17d5d9276a227b13aa
Yunyoung Chung <double.y.0525@gmail.com>
2020-10-25 21:43:49 +0900

    BOJ 2805 - 나무 자르기(python)

--- BOJ/boj_2805_cutting_tree.py
+++ BOJ/boj_2805_cutting_tree.py
@@ -2,4 +2,20 @@ import sys
 sys.stdin = open('../input.txt', 'r')
 
 N, M = map(int, input().split())
-h = list(map(int, input().split()))
+trees = list(map(int, input().split()))
+min_height, max_height = 1, max(trees)
+
+while min_height <= max_height:
+    total_tree = 0
+    cutting_height = (max_height + min_height) // 2
+
+    for tree in trees:
+        if tree > cutting_height:
+            total_tree += tree - cutting_height
+
+    if total_tree >= M:
+        min_height = cutting_height + 1
+    elif total_tree < M:
+        max_height = cutting_height - 1
+
+print(max_height)
